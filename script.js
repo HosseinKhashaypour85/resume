e3f0b05e470c275105243683204075030004273b
HosseinKhashaypour <hosseinkhashaypour85@gmail.com>
2024-10-16 18:18:08 +0330

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 const dom = document.body;
-const trueerror = document.querySelector("#error");
-const flaseerror = document.querySelector("#flase-error");
-const theme = document.querySelector("#theme img");
-const themesta = document.querySelector(".light-icon");
-const menu = document.querySelector("span");
-const dnresume = document.querySelector("#resume > .resume");
+const themeSta = document.querySelector('.dark-icon');
 
 
 
@@ -22,18 +17,88 @@ const dnresume = document.querySelector("#resume > .resume");
 //     }
 // });
 function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-  }
-  
-  function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-  }
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
 
 
-window.addEventListener('offline' , (e)=>{
-  return 'اتصال شما قطع شده است';
+
+const resumeBtn = document.querySelector('#resumeButton');
+resumeBtn.addEventListener('click' , ()=>{
+  let samples = document.querySelector('#samples')
+  samples.scrollIntoView({behavior : "smooth"})
 })
 
-window.addEventListener('online' ,  (e)=>{
-  return 'اتصال شما برقرار شد'
+
+
+// objects for colors
+
+
+
+const lightModeColors = {
+  backgroundColors : 'white',
+  textColors : 'black',
+  containerTextColors : 'white',
+  containerColors : 'gray',
+}
+
+const darkModeColors = {
+  backgroundColors : 'rgb(15 23 42)',
+  textColors : 'white',
+  containerTextColors : 'white',
+  containerColors : 'gray',
+}
+
+document.body.style.backgroundColor = lightModeColors.backgroundColors
+document.body.style.color = lightModeColors.textColors;
+
+const allPTags = document.querySelectorAll('p, p1 , p2');
+
+const allHTags = document.querySelectorAll('h1');
+
+
+
+
+const storedTheme = localStorage.getItem('theme')
+let currentMode = localStorage.getItem('theme') || 'light';
+const allServicesDetails = document.querySelectorAll(".services-deatail")
+
+
+
+const darkIcon = document.querySelector('.dark-icon');
+
+function applyTheme (){
+  if(currentMode === 'dark'){
+    allServicesDetails.forEach(serviceDeatail=>{
+      serviceDeatail.style.color = darkModeColors.textColors
+    })
+    allPTags.forEach(pTag=>{
+      pTag.style.color = lightModeColors.textColors
+    })
+    document.body.style.backgroundColor = lightModeColors.backgroundColors
+    document.body.style.color = lightModeColors.textColors;
+    darkIcon.src = 'dark-mode.png'
+    // currentMode = 'light'
+  } else{
+    allPTags.forEach(pTag=>{
+      pTag.style.color = darkModeColors.textColors
+    })
+    document.body.style.backgroundColor = darkModeColors.backgroundColors
+    document.body.style.color = darkModeColors.textColors;
+    darkIcon.src = 'light-mode.png'
+    // currentMode = 'dark'
+  }
+  localStorage.setItem('theme' , currentMode)
+}
+
+window.addEventListener('load' , ()=>{
+  applyTheme()
+})
+
+darkIcon.addEventListener('click', ()=>{
+  currentMode = (currentMode === "dark") ? 'light' : 'dark';
+  applyTheme()
 })
